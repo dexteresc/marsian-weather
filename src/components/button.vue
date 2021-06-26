9c742ea5e70b1e626677218194f7fee63ad72fc2
jonat <jonathan.jeding@gmail.com>
2021-06-26 22:03:50 +0200

    Updated generic button to work in all current scenarios.

--- src/components/button.vue
+++ src/components/button.vue
@@ -6,6 +6,18 @@
 <script>
 export default {
   name: "Button",
+  props: {
+    disabled: Boolean,
+  },
+  methods: {
+    errorAnimation() {
+      const element = document.getElementsByClassName('error')
+
+      element.classList.remove('classname'); // reset animation
+      void element.offsetWidth; // trigger reflow
+      element.classList.add('classname'); // start animation
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
@@ -38,36 +50,18 @@ button {
     background-color: $accent-color;
 
   }
+
   &.rounded {
     border-radius: 50%;
     padding: 0;
   }
-  &.error {
-    color: #f44336;
-    &:active {
-      animation: shake 0.82s cubic-bezier(.36, .07, .19, .97) both;
-      backface-visibility: hidden;
-    }
+
+  &.active {
+    color: blue;
   }
 }
 
-@keyframes shake {
-  10%, 90% {
-    transform: translate3d(-1px, 0, 0);
-  }
 
-  20%, 80% {
-    transform: translate3d(2px, 0, 0);
-  }
-
-  30%, 50%, 70% {
-    transform: translate3d(-4px, 0, 0);
-  }
-
-  40%, 60% {
-    transform: translate3d(4px, 0, 0);
-  }
-}
 
 
 </style>
